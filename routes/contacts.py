adcb50913f3e7b875ef0e40c3630ae03c3936160
EstebanIslas <1717110261@utectulancingo.edu.mx>
2024-02-13 16:00:03 -0600

    Actualizando datos backend

--- routes/contacts.py
+++ routes/contacts.py
@@ -22,11 +22,22 @@ def add_contacts():
 
     return redirect(url_for('contacts.home'))
 
-@contacts.route('/update')
-def update_contacts():
-    return 'Update contacts'
+@contacts.route('/update/<id>', methods=['POST', 'GET'])
+def update_contacts(id):
 
-@contacts.route('/delete/<id>')
+    contact = Contact.query.get(id)
+    if request.method == 'POST':
+        contact.fullname = request.form['fullname']
+        contact.email = request.form['email']
+        contact.phone = request.form['phone']
+
+        db.session.commit()
+
+        return redirect(url_for('contacts.home'))
+
+    return render_template('update.html', contact = contact)
+
+@contacts.route('/delete/<id>', methods = ['POST', 'GET'])
 def delete_contacts(id):
     contact = Contact.query.get(id)
     db.session.delete(contact)
