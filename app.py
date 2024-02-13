a62f14093a829bc7b0befa8815a09a71708bae5a
EstebanIslas <1717110261@utectulancingo.edu.mx>
2024-02-13 12:33:13 -0600

    Finalizando estructura

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
+#Config de la aplicacion
+
 from flask import Flask
 
 app = Flask(__name__)
 
-if __name__ == "__main__":
-    app.run()
-    
+@app.route('/')
+def home():
+    return 'Hello'
+
---
+++ index.py
@@ -0,0 +1,6 @@
+# Archivo que arranca la app
+from app import app
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
